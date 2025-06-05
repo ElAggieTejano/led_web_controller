2d7d02dd32e03931083859389eadf85aa6ee3255
ElAggieTejano <luis.oro08@outlook.com>
2025-06-05 12:04:34 -0500

    add ino file to project

---
+++ led_web_controller.ino
@@ -0,0 +1,114 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <FastLED.h>
+
+// WiFi Access Point config
+const char *ssid = "ESP32_LED_Controller";
+const char* password = "12345678";
+IPAddress local_ip(192, 98, 24, 1);
+IPAddress gateway(192, 98, 24, 1);
+IPAddress subnet(255, 255, 255, 0);
+WebServer server(80);
+unsigned long lastToggle = 0;
+bool cautionState = false;
+
+// LED config
+#define LED_PIN_STRIP1 5
+#define LED_PIN_STRIP2 18
+#define NUM_LEDS 30
+CRGB leds1[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+
+enum LEDMode { OFF, ON, AMBER, RED, CAUTION };
+LEDMode strip1Mode = OFF;
+LEDMode strip2Mode = OFF;
+
+
+void setupWiFi() {
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+}
+
+void handleRoot() {
+  server.send(200, "text/html", R"rawliteral(
+    <html>
+    <head><title>ESP32 LED Control</title></head>
+    <body style="text-align:center;">
+      <h1>Strip 1 Control</h1>
+      <button onclick="location.href='/strip1/on'">ON</button>
+      <button onclick="location.href='/strip1/off'">OFF</button>
+      <button onclick="location.href='/strip1/amber'">Amber</button>
+      <button onclick="location.href='/strip1/red'">Red</button>
+      <button onclick="location.href='/strip1/caution'">Caution</button>
+      <h1>Strip 2 Control</h1>
+      <button onclick="location.href='/strip2/on'">ON</button>
+      <button onclick="location.href='/strip2/off'">OFF</button>
+      <button onclick="location.href='/strip2/amber'">Amber</button>
+      <button onclick="location.href='/strip2/red'">Red</button>
+    </body>
+    </html>
+  )rawliteral");
+}
+
+void applyLEDMode(CRGB *leds, LEDMode mode) {
+  switch (mode) {
+    case ON:
+      fill_solid(leds, NUM_LEDS, CRGB::White);
+      break;
+    case AMBER:
+      fill_solid(leds, NUM_LEDS, CRGB(255, 100, 0));
+      break;
+    case RED:
+      fill_solid(leds, NUM_LEDS, CRGB::Red);
+      break;
+    case OFF:
+      fill_solid(leds, NUM_LEDS, CRGB::Black);
+      break;
+    case CAUTION:
+      // Caution handled in loop (flashing state)
+      break;
+  }
+  FastLED.show();
+}
+
+void defineRoutes() {
+  server.on("/", handleRoot);
+
+  server.on("/strip1/on", []() { strip1Mode = ON; applyLEDMode(leds1, ON); server.sendHeader("Location", "/"); server.send(303); });
+  server.on("/strip1/off", []() { strip1Mode = OFF; applyLEDMode(leds1, OFF); server.sendHeader("Location", "/"); server.send(303); });
+  server.on("/strip1/amber", []() { strip1Mode = AMBER; applyLEDMode(leds1, AMBER); server.sendHeader("Location", "/"); server.send(303); });
+  server.on("/strip1/red", []() { strip1Mode = RED; applyLEDMode(leds1, RED); server.sendHeader("Location", "/"); server.send(303); });
+  server.on("/strip1/caution", []() { strip1Mode = CAUTION; server.sendHeader("Location", "/"); server.send(303); });
+
+  server.on("/strip2/on", []() { strip2Mode = ON; applyLEDMode(leds2, ON); server.sendHeader("Location", "/"); server.send(303); });
+  server.on("/strip2/off", []() { strip2Mode = OFF; applyLEDMode(leds2, OFF); server.sendHeader("Location", "/"); server.send(303); });
+  server.on("/strip2/amber", []() { strip2Mode = AMBER; applyLEDMode(leds2, AMBER); server.sendHeader("Location", "/"); server.send(303); });
+  server.on("/strip2/red", []() { strip2Mode = RED; applyLEDMode(leds2, RED); server.sendHeader("Location", "/"); server.send(303); });
+}
+
+void setup() {
+  Serial.begin(115200);
+  setupWiFi();
+  defineRoutes();
+  server.begin();
+
+  FastLED.addLeds<WS2811, LED_PIN_STRIP1, GRB>(leds1, NUM_LEDS);
+  FastLED.addLeds<WS2811, LED_PIN_STRIP2, GRB>(leds2, NUM_LEDS);
+
+  FastLED.clear(true);
+}
+
+void loop() {
+  server.handleClient();
+
+  // Flash amber at 2Hz if in CAUTION mode
+  if (strip1Mode == CAUTION) {
+    unsigned long now = millis();
+    if (now - lastToggle >= 500) {
+      cautionState = !cautionState;
+      fill_solid(leds1, NUM_LEDS, cautionState ? CRGB(255, 100, 0) : CRGB::Black);
+      FastLED.show();
+      lastToggle = now;
+    }
+  }
+}
